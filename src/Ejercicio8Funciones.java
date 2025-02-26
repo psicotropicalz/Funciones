312d73d8d976d43815e1027989c17fb25b3525d0
psicotropicalz <somejazz@hotmail.com>
2025-02-26 21:01:11 +0100

    Ejercicios

--- src/Ejercicio8Funciones.java
+++ src/Ejercicio8Funciones.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 /**
  * 1. Implementa una función convertirMoneda(), que reciba una cantidad de dólares Canadienses o Americanos y la convierta en euros
  * 2. Implementa una función convertirGradosCelsius() que reciba en ºF y los convierta en ºC
@@ -11,8 +10,9 @@ import java.util.Scanner;
  */
 
 public class Ejercicio8Funciones {
+    static String mensajeEuros = "Dime cuántos euros quieres convertir";
     public static void main(String[] args) {
-
+        menu();
     }
     static double solicitarNumero(String mensaje) {
         Scanner entrada = new Scanner(System.in);
@@ -20,16 +20,16 @@ public class Ejercicio8Funciones {
         return entrada.nextDouble();
     }
     static double convertirDolaresA () {
-        double dolares = solicitarNumero("Dime cuántos dólares quieres convertir");
-        return (dolares * 1.05);
+        double euros = solicitarNumero(mensajeEuros);
+        return (euros * 1.05);
     }
     static double convertirDolaresC () {
-        double dolares = solicitarNumero("Dime cuántos dólares quieres convertir");
-        return (dolares * 1.5);
+        double euros = solicitarNumero(mensajeEuros);
+        return (euros * 1.5);
     }
     static double convertirAGradosCelsius (){
         double fahrenheit = solicitarNumero("Cuántos grados Fahrenheit hay");
-        return (fahrenheit - 32) * (5 / 9);
+        return (fahrenheit - 32) * ((double) 5 / 9);
     }
     static double convertirAGradosFahrenheit (){
         double celsius = solicitarNumero("Cuántos grados Celsius hay");
@@ -39,50 +39,113 @@ public class Ejercicio8Funciones {
         double metros = solicitarNumero("Cuántos metros quieres convertir a milímetros");
         return (metros * 1000);
     }
-    static double convertirALibras () {
-        double kg = solicitarNumero("Cuántos kg quieres convertir");
-        return (kg / 2.205);
+    static double convertirMasa (int numero) {
+        double resultado = 0;
+        if (numero == 1) {
+            double kg = solicitarNumero("Cuántos kg quieres convertir");
+            resultado = (kg / 2.205);
+        } else if (numero == 2) {
+            double libras = solicitarNumero("Cuántas libras quieres convertir");
+            resultado = (libras * 2.205);
+        }
+        return resultado;
     }
-    static double convertirAKilos () {
-        double libras = solicitarNumero("Cuántas libras quieres convertir");
-        return (libras * 2.205);
+    static double convertirDistancia (int numero) {
+        double resultado = 0;
+        if (numero == 1) {
+            double millas = solicitarNumero("Cuántas millas quieres convertir");
+            resultado = (millas * 1.609);
+        } else if (numero == 2) {
+            double km = solicitarNumero("Cuántos kilómetros quieres convertir");
+            resultado = (km / 1.609);
+        }
+        return resultado;
     }
+    static String convertirSegundos () {
+        double segundos = (int)solicitarNumero("Cuántos segundos quieres convertir");
+        int minutos = 0, horas = 0, dias = 0, semanas = 0;
+        String mensaje;
+        if (segundos >= 60) {
+            minutos = (int) segundos / 60;
+            segundos = segundos % 60;
+        }
+        if (minutos >= 60) {
+            horas = minutos / 60;
+            minutos = minutos % 60;
+        }
+        if (horas >= 24) {
+            dias = horas / 24;
+            horas = horas % 24;
+        }
+        if (dias >= 7) {
+            semanas = dias / 7;
+            dias = dias % 7;
+        }
+        return "\nLa cantidad de segundos que has introducido son: " + semanas + " semanas - " + dias + " dias - " + horas + " horas - " + minutos + " minutos - " + (int)segundos + " segundos";
+    }
+
     static void menu (){
         Scanner entrada = new Scanner(System.in);
-        String opcion = "";
+        String opcion, subOpcion;
         do {
-            System.out.print("\n[1]-> Convertir monedas \n[2]-> Convertir temperatura\n[3]-> Multiplicar 2 números\n[4]-> Dividir 2 números\n[5]-> Salir\nElije qué operación quieres realizar: ");
+            System.out.print("\n[1]-> Convertir monedas \n[2]-> Convertir temperatura\n[3]-> Convertir a milímetros\n[4]-> Convertir masa\n[5]-> Convertir distancias\n[6]-> Convertir cantidad de segundos\n[7]-> Calcular la hora de llegada\n[8]-> Salir\nElije qué operación quieres realizar: ");
             opcion = entrada.nextLine();
             switch (opcion) {
                 case "1" -> {
                     do {
-                        System.out.println("\n[1]-> Dólares Estadounidenses\n[2]-> Dólares Canadienses\n[3]-> Salir \nIndica qué tipo de moneda quieres convertir: ");
-                        String moneda = entrada.nextLine();
-                        switch (moneda) {
+                        System.out.print("\n[1]-> Dólares Estadounidenses\n[2]-> Dólares Canadienses\n[3]-> Salir \nIndica qué tipo de moneda quieres convertir: ");
+                        subOpcion = entrada.nextLine();
+                        switch (subOpcion) {
                             case "1" -> System.out.println("\nEso serían " + convertirDolaresA() + " dólares Estadounidenses.");
                             case "2" -> System.out.println("\nEso serían " + convertirDolaresC() + " dólares Canadienses.");
-                            case "3" -> System.out.println("Adiós! :)");
+                            case "3" -> System.out.println("\nVolviendo al menú principal...");
                             default -> System.out.println("\nNo has seleccionado una opción válida");
                         }
-                    } while (!opcion.equals("3"));
+                    } while (!subOpcion.equals("3"));
                 }
                 case "2" -> {
                     do {
-                        System.out.println("\n[1]-> Convertir grados Fahrenheit a Celsius\n[2]-> Convertir grados Celsius a Fahrenheit\n[3]-> Salir \nIndica qué temperatura quieres convertir: ");
-                        String moneda = entrada.nextLine();
-                        switch (moneda) {
+                        System.out.print("\n[1]-> Convertir grados Fahrenheit a Celsius\n[2]-> Convertir grados Celsius a Fahrenheit\n[3]-> Salir \nIndica qué temperatura quieres convertir: ");
+                        subOpcion = entrada.nextLine();
+                        switch (subOpcion) {
                             case "1" -> System.out.println("\nEso serían " + convertirAGradosCelsius() + " º Celsius.");
                             case "2" -> System.out.println("\nEso serían " + convertirAGradosFahrenheit() + " º Fahrenheit.");
-                            case "3" -> System.out.println("Adiós! :)");
+                            case "3" -> System.out.println("\nVolviendo al menú principal...");
                             default -> System.out.println("\nNo has seleccionado una opción válida");
                         }
-                    } while (!opcion.equals("3"));
+                    } while (!subOpcion.equals("3"));
                 }
                 case "3" -> System.out.println("\nEso serían " + convertirMilimetros() + " milímetros.");
-                //case "4" -> System.out.println("\nEl resultado de la división sería: " + division(solicitarNumero(), solicitarNumero()));
-                case "5" -> System.out.println("Adiós! :)");
+                case "4" -> {
+                    do {
+                        System.out.print("\n[1]-> Convertir Kilogramos a Libras\n[2]-> Convertir Libras a Kilogramos\n[3]-> Salir \nIndica qué masa quieres convertir: ");
+                        subOpcion = entrada.nextLine();
+                        switch (subOpcion) {
+                            case "1" -> System.out.println("\nEso serían " + convertirMasa(2) + " Kg.");
+                            case "2" -> System.out.println("\nEso serían " + convertirMasa(1) + " Libras.");
+                            case "3" -> System.out.println("\nVolviendo al menú principal...");
+                            default -> System.out.println("\nNo has seleccionado una opción válida");
+                        }
+                    } while (!subOpcion.equals("3"));
+                }
+                case "5" -> {
+                    do {
+                        System.out.print("\n[1]-> Convertir Millas a Kilómetros\n[2]-> Convertir Kilómetros a Millas\n[3]-> Salir \nIndica qué distancia quieres convertir: ");
+                        subOpcion = entrada.nextLine();
+                        switch (subOpcion) {
+                            case "1" -> System.out.println("\nEso serían " + convertirDistancia(1) + " kilómetros.");
+                            case "2" -> System.out.println("\nEso serían " + convertirDistancia(2) + " millas.");
+                            case "3" -> System.out.println("\nVolviendo al menú principal...");
+                            default -> System.out.println("\nNo has seleccionado una opción válida");
+                        }
+                    } while (!subOpcion.equals("3"));
+                }
+                case "6" -> System.out.println(convertirSegundos());
+                case "7" -> Ejercicio7Funciones.calcularLlegada();
+                case "8" -> System.out.println("\n\nAdiós! :)");
                 default -> System.out.println("\nNo has seleccionado una opción válida");
             }
-        } while (!opcion.equals("5"));
+        } while (!opcion.equals("8"));
+        System.out.println("\nPrograma desarrollado por Daniel Cano");
     }
 }
